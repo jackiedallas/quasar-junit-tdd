a451290a6958f7631b2e582c08eb46af7ba05316
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-11-15 09:49:40 -0600

    ran hello world example test

---
+++ src/test/java/TestingWithJUnit.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestingWithJUnit {
+
+    @Test
+    public void testHelloWorld() {
+        System.out.println("Hello World");
+    }
+
+
+
+
+
+}
